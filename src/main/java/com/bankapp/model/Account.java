1808afebe459d09c34e7ad75223890d9ec1c42cc
robingarhwal08 <robingarhwal86@gmail.com>
2025-06-22 09:47:35 +0530

    Create Account.java

---
+++ src/main/java/com/bankapp/model/Account.java
@@ -0,0 +1,12 @@
+package com.bankapp.model;
+
+import java.math.BigDecimal;
+
+public class Account {
+    private Long id;
+    private Long customerId;
+    private String accountNumber;
+    private BigDecimal balance;
+
+    // Getters and setters
+}
